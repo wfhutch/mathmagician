0b1eb07b45df1bd4e70201d40988d29480bc4707
wfhutch <wfh2727@gmail.com>
2015-10-23 21:06:31 -0500

    added OddNumbers class

---
+++ Mathmagician/OddNumbers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathmagician
+{
+    public class OddNumbers : NaturalNumbers
+    {
+        public OddNumbers()
+        {
+            first = 1;
+            increment = 2;
+        }
+    }
+}
